8cfaca2e787d1be6fa34e379dbdda2543653a33b
CDPS <crispa_07@hotmail.com>
2015-10-12 15:58:59 -0500

    Reparo de errores

--- application/views/crud_Vehiculos.php
+++ application/views/crud_Vehiculos.php
@@ -1,24 +1,4 @@
 
-<h3>Vehiculos</h3>
-
-<div class="dtabla">
-	<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>Referencia</th>
-						<th>Placa</th>
-						<th>Capacidad Máxima</th>
-					</tr>	
-				</thead>
-
-				<tbody>
-		
-				</tbody>
-	</table>
-
-<head>
-    <script src="application/js/crd.js"></script>
-</head>
 
 <h3>Vehiculos</h3>
 
