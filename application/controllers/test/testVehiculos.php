65b94c3f35ead434f86193b032edd2b399012fcb
CDPS <crispa_07@hotmail.com>
2015-10-12 10:54:57 -0500

    Carpeta para pruebas unitarias
    Se crea una carpeta para las pruebas unitarias

--- application/controllers/test/testVehiculos.php
+++ application/controllers/test/testVehiculos.php
@@ -1,18 +1,17 @@
 <?php
  
-class TestVehiculo extends CI_Controller {
+class TestVehiculos extends CI_Controller {
  
     public function __construct()
     {
         parent::__construct();
         $this->load->library('unit_test');
         $this->load->model('vehiculo');
-       
     }
 
-    public function index(){
-    	echo "prueba";
-    }
-
-
+    public function index()
+	{
+		echo "hola";
+        
+	}
 }
